2bb4543cafd35b8d571a942ad38af91bcaa234b3
Kalinina Antonina <staraya.kalinina@yandex.ru>
2024-04-21 19:30:34 +0200

    Added img for message

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const config = require('config'); //подключаем пакет config
 const TelegramBot = require('node-telegram-bot-api');
+const config = require('config');
+const fs = require('fs');
 
 const bot = new TelegramBot(config.get('telegram.token'), {polling: false});
-const text = 'Заполните ТШ!';
+const {
+    text,
+    stage,
+    link
+} = config.get('work');
 
-bot.sendMessage(config.get('telegram.chatId'), text);
+const message = `${text}\n${stage}\n${link}`;
 
+const fileName = 'indexImg.txt';
+const picturesPath = './pictures';
+let indexPicture;
+const pictures = fs.readdirSync(`${picturesPath}`).map(a => a.split('.'));
+
+const isExist = fs.existsSync(`${fileName}`);
+
+if (isExist) {
+    indexPicture = fs.readFileSync(`${fileName}`,'utf8');
+
+    if(Number(indexPicture) >= pictures.length - 1) {
+        indexPicture = 0;
+    } else {
+        indexPicture = Number(indexPicture) + 1;
+    }
+    fs.writeFileSync(`${fileName}`, `${indexPicture}`);
+} else {
+    //Для первой записи
+    fs.writeFileSync(`${fileName}`, '0');
+}
+
+const imgWithSort = pictures.sort(([a], [c]) => [Number(a)] - [Number(c)]);
+const [a, b] = imgWithSort[indexPicture];
+const imgPath = `${picturesPath}/${a}.${b}`;
+
+
+bot.sendPhoto(config.get('telegram.chatId'), `${imgPath}`, options = {caption : message});
