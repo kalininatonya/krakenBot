612d178bf3ee1c53a3b2f73e8f1cc2d60c54e842
Kalinina Antonina <staraya.kalinina@yandex.ru>
2024-04-21 19:45:38 +0200

    fixed variables names

--- index.js
+++ index.js
@@ -14,27 +14,27 @@ const message = `${text}\n${stage}\n${link}`;
 const fileName = 'indexImg.txt';
 const picturesPath = './pictures';
 let indexPicture;
-const pictures = fs.readdirSync(`${picturesPath}`).map(a => a.split('.'));
+const pictures = fs.readdirSync(picturesPath).map(a => a.split('.'));
 
-const isExist = fs.existsSync(`${fileName}`);
+const isExist = fs.existsSync(fileName);
 
 if (isExist) {
-    indexPicture = fs.readFileSync(`${fileName}`,'utf8');
+    indexPicture = fs.readFileSync(fileName,'utf8');
 
     if(Number(indexPicture) >= pictures.length - 1) {
         indexPicture = 0;
     } else {
         indexPicture = Number(indexPicture) + 1;
     }
-    fs.writeFileSync(`${fileName}`, `${indexPicture}`);
+    fs.writeFileSync(fileName, `${indexPicture}`);
 } else {
     //Для первой записи
-    fs.writeFileSync(`${fileName}`, '0');
+    fs.writeFileSync(fileName, '0');
 }
 
-const imgWithSort = pictures.sort(([a], [c]) => [Number(a)] - [Number(c)]);
-const [a, b] = imgWithSort[indexPicture];
-const imgPath = `${picturesPath}/${a}.${b}`;
+const imgWithSort = pictures.sort(([name1], [name2]) => [Number(name1)] - [Number(name2)]);
+const [name, extension] = imgWithSort[indexPicture];
+const imgPath = `${picturesPath}/${name}.${extension}`;
 
 
 bot.sendPhoto(config.get('telegram.chatId'), `${imgPath}`, options = {caption : message});
