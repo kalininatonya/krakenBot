d512b74913c910877d1c7fea8cf53d5b37504e5e
Kalinina Antonina <staraya.kalinina@yandex.ru>
2024-11-21 22:12:38 +0200

    added birthday greetings

---
+++ constants.mjs
@@ -0,0 +1,21 @@
+export const months = {
+    янв: 0,
+    фев: 1,
+    мар: 2,
+    апр: 3,
+    мая: 4,
+    июн: 5,
+    июл: 6,
+    авг: 7,
+    сен: 8,
+    окт: 9,
+    ноя: 10,
+    дек: 11,
+};
+
+export const tableTitles = {
+    nameTitle: {id: 1, title: 'ФИ %'},
+    birthdayTitle: {id: 2, title: 'день рождения'},
+    contactTitle: {id: 3, title: 'контакт'}
+};
+
